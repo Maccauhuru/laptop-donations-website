4cbfff3a3a5e66a829db6d253384935136356e7f
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-21 21:16:56 -0500

    restarted from scratch

--- src/app.js
+++ src/app.js
@@ -1,37 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import { BrowserRouter, Route, Switch} from "react-router-dom";
-import Navigation from './components/Navigation';
-import Hero from './components/Hero';
-import UsageSection from './components/Usage-Section';
-import PreviousDonations from './components/PreviousDonations';
-import SignUpSection from './components/SignUpSection';
-import Footer from './components/Footer';
-import AppRouter from './routers/AppRouter';
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './Components/Navigation';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Navigation />
+      </div>
+    );
+  }
+}
 
-const App = ()=>{
-return(
-    <div>
-        <Navigation />
-        <Hero />
-        <UsageSection />
-        <PreviousDonations />
-        <SignUpSection />
-        <Footer />
-    </div>
-        )
-    }
-
-const jsx = (
-    <BrowserRouter >
-    <App />
-    </BrowserRouter>
-);
-
-
-let appRoot = document.getElementById("app-info");
-ReactDOM.render(jsx,appRoot);
-
+export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,31 +1,64 @@
 import React from 'react';
-import { NavLink } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-const divStyle = {
-    background: 'black',
-    border: '1px solid white',
-    textAlign: 'center',
-    color:'white'
-};
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
 
-const Navigation = ()=>{
-    return(<div style={divStyle}>
-    <header>
-      <h1>Laptop Donations App</h1>
-    </header>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Home
-    </NavLink>
-    <NavLink to="/how-it-works" activeClassName="is-active">
-      How It Works
-    </NavLink>
-    <NavLink to="/faq" activeClassName="is-active">
-      Faq
-    </NavLink>
-      <NavLink to="/contact" activeClassName="is-active">
-      Contact
-    </NavLink>
-        </div>)
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div className="nav-component">
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Laptop-Donations.Org</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">How it works</NavLink>
+              </NavItem>
+             <NavItem>
+                <NavLink href="/components/">FAQ</NavLink>
+              </NavItem>
+             <NavItem>
+                <NavLink href="/components/">Contact</NavLink>
+              </NavItem>
+              <div className="nav-divider">|</div>
+             <NavItem>
+            <NavLink href="/components/">Login</NavLink>
+              </NavItem>
+            <NavItem>
+            <NavLink href="/components/">Sign Up</NavLink>
+              </NavItem>
+             
+                
+                
+
+
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
-export default Navigation;
